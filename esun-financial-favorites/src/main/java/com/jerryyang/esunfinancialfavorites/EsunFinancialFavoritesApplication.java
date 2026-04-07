58bd00fbc27ddb20b16d908256f5b542585ea3b0
JerryYang <jerry1020755@gmail.com>
2026-04-07 21:31:56 +0800

    初始化 Spring boot project

---
+++ esun-financial-favorites/src/main/java/com/jerryyang/esunfinancialfavorites/EsunFinancialFavoritesApplication.java
@@ -0,0 +1,13 @@
+package com.jerryyang.esunfinancialfavorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsunFinancialFavoritesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsunFinancialFavoritesApplication.class, args);
+    }
+
+}
---
+++ esun-financial-favorites/src/test/java/com/jerryyang/esunfinancialfavorites/EsunFinancialFavoritesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerryyang.esunfinancialfavorites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsunFinancialFavoritesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
